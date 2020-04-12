cab66c1f5ccdb106b4cc3a515fbe943c08e24fef
Rafasuafer <rafaelsuarez.dev@gmail.com>
2020-04-11 21:07:34 -0300

    Logica Menu - BASE
    Agrego la navegacion basica del menu.

--- n2cOblP2/COVIDA/Menu.cs
+++ n2cOblP2/COVIDA/Menu.cs
@@ -11,30 +11,91 @@ namespace COVIDA
 		// Atributos
 		private bool salir;
 		// Propiedades
-		public bool Salir{
-			get { return salir; }
-		}
+		
+
 		// Builder
 		public Menu()
 		{
 			salir = false;
 		}
-
+		
 		#region Metodos
+
+		public void bienvenida(){
+			Console.WriteLine("\n##############\n### COVIDA ###\n##############");
+			Console.WriteLine("|--------------------------|");
+			Console.WriteLine("| Elianna Fiorina - 181332 |");
+			Console.WriteLine("| Rafael  Suarez  - 250827 |");
+			Console.WriteLine("|--------------------------|");
+		}
+
 		public void inicio()
 		{
-			Console.WriteLine("##############\n### COVIDA ###\n##############");
-			Console.WriteLine("\n");
-		}
-		public void mostrarOpciones()
-		{
-			Console.WriteLine("s - CERRAR");
+			
+			while (!salir)
+			{
+				Console.WriteLine("\n### INICIO ###");
+				mostrarOpciones();
+				string input = pedirInput("Elija una Opcion:");
+				switch (input)
+				{
+					case "1":
+						donaciones();
+						break;
+					case "2":
+						centros();
+						break;
+					case "3":
+						buscarDonaciones();
+						break;
+					case "s":
+						cerrar();
+						break;
+					default:
+						break;
+				}
+
+			}
 		}
 
-		public void cerrar(){
-			this.salir = false;
+		
+		public void donaciones(){
+			Console.WriteLine("\n### DONAR ###");
+		}
+
+		public void centros()
+		{
+			Console.WriteLine("\n### CENTROS ###");
+
+		}
+
+		public void buscarDonaciones(){
+			Console.WriteLine("\n### BUSCAR DONACION ###");
+		}
+
+		public void mostrarOpciones()
+		{
+			Console.WriteLine("1 - Donar");
+			Console.WriteLine("2 - Ver Centros");
+			Console.WriteLine("3 - Ver Donaciones");
+			Console.WriteLine("s - SALIR");
+		}
+
+		public string pedirInput(string msg){
+			Console.WriteLine(msg);
+			string input = Console.ReadLine();
+
+			return input;
+		}
+
+		public void cerrar() {
+			Console.WriteLine("Adios!");
+			this.salir = true;
 		}
 
 		#endregion
+
+		
+
 	}
 }
--- n2cOblP2/COVIDA/Program.cs
+++ n2cOblP2/COVIDA/Program.cs
@@ -5,29 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace COVIDA
-{
+{	
 	class Program
-	{
+	{ 
 		static void Main(string[] args)
 		{
 			Menu menu = new Menu();
-			string input;
+			menu.bienvenida();
 
 			menu.inicio();
-			while (!menu.Salir)
-			{
-				menu.mostrarOpciones();
-				input = Console.ReadLine();
-				switch (input)
-				{
-					case "c":
-						menu.cerrar();
-						break;
-					default:
-						break;
-				}
-
-			}
+			
 			Console.ReadKey();
 		}
 	}
